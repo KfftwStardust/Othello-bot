aa54375897176a8c7ecad0a68cc5710d6cc299e9
AdamCigehn <111567363+AdamCigehn@users.noreply.github.com>
2023-11-29 13:12:20 +0000

    valid moves

--- Othello.py
+++ Othello.py
@@ -20,12 +20,59 @@ def changeBoard(pos,board,currentplayer):
     board[pos1] = temp
     return board
 
+def get_possible_moves(board, currentplayer):
+    possible_moves = []
+
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == 0:
+                if is_valid_move(board, i, j, currentplayer):
+                    possible_moves.append((i, j))
+
+    return possible_moves
+
+def is_valid_move(board, row, col, currentplayer):
+    if board[row][col] != 0:
+        return False
+
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]
+
+    for direction in directions:
+        if is_valid_direction(board, row, col, direction, currentplayer):
+            return True
+
+    return False
+
+def is_valid_direction(board, row, col, direction, currentplayer):
+    opponent = 3 - currentplayer
+    i, j = direction
+
+    x, y = row + i, col + j
+    if not (0 <= x < 8 and 0 <= y < 8) or board[x][y] != opponent:
+        return False
+
+    x += i
+    y += j
+
+    while 0 <= x < 8 and 0 <= y < 8:
+        if board[x][y] == currentplayer:
+            return True
+        elif board[x][y] == 0:
+            return False
+
+        x += i
+        y += j
+
+    return False
+
 changeBoard(33,board,1)
 changeBoard(44,board,1)
 changeBoard(43,board,2)
 changeBoard(34,board,2)
 printBoard(board)
 while True:
+    possible_moves = get_possible_moves(board, currentplayer)
+    print("Possible moves:", possible_moves)
     pos=int(input("Vilken pos 11 till 88 "))-11
     while pos%10 >7 or pos > 77:
         pos=int(input("Vilken pos 11 till 88 "))-11
